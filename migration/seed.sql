54b009186cafc85dbcb2f709f2c39f38b9419a4d
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-29 21:00:04 +0800

    feat : database seeder

---
+++ migration/seed.sql
@@ -0,0 +1,5 @@
+USE museum_db;
+
+INSERT INTO collections (name, origin, description, year_found, photo_url) VALUES
+('Gamelan Jawa', 'Yogyakarta', 'Alat musik tradisional dari Jawa yang terdiri dari gong, kenong, dan bonang.', 1850, 'https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/133/2024/01/25/Bonang-199555460.jpg'),
+('Topeng Barong', 'Bali', 'Topeng tradisional yang digunakan dalam tarian Barong sebagai simbol pelindung.', 1920, 'https://www.wonderverseindonesia.com/storage/app/media/uploaded-files/unnamed-3.png');
